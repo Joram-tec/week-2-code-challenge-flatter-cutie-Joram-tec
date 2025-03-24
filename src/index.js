958503d20b74dcbdc3de8523bf0246ab1ceb0b25
Joram Wayne <joramwmuya@gmail.com>
2025-03-24 16:08:05 +0300

    Complete Images posted

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
             span.addEventListener("click", () =>
 
             displayCharacterDetails(character)
-            
+
           );
           characterBar.appendChild(span);
         });
@@ -27,46 +27,58 @@ document.addEventListener("DOMContentLoaded", () => {
       .catch((error) => console.error("Error fetching characters:", error));
   });
   function displayCharacterDetails(character) {
-    const detailedInfo = document.getElementById("detailed-info");
-    detailedInfo.innerHTML = `
-      <img src="${character.image}" alt="${character.name}" />
-      <h2>${character.name}</h2>
-      <p>Votes: <span id="vote-count">${character.votes}</span></p>
+
+     const detailedInfo = document.getElementById("detailed-info");
+
+    detailedInfo.innerHTML = `<img src="${character.image}" alt="${character.name}" />
+    <h2>${character.name}</h2>
+        <p>Votes: <span id="vote-count">${character.votes}</span></p>
     `;
   
     
     const votesForm = document.getElementById("votes-form");
+
     votesForm.addEventListener("submit", (event) => {
       event.preventDefault();
-      const votesInput = document.getElementById("votes");
-      const voteCount = document.getElementById("vote-count");
-      const newVotes =
-        parseInt(voteCount.textContent) + parseInt(votesInput.value);
+     
+        const votesInput = document.getElementById("votes");
+
+            const voteCount = document.getElementById("vote-count");
+      const newVotes =  parseInt(voteCount.textContent) + parseInt(votesInput.value);
+
       voteCount.textContent = newVotes;
+
       votesForm.reset();
     });
   }
   
-  const resetButton = document.getElementById('reset-btn');
+  
+const resetButton = document.getElementById('reset-btn');
+
   resetButton.addEventListener('click', () => {
+
     const voteCount = document.getElementById('vote-count');
+
     voteCount.textContent = 0;
   });
   const characterForm = document.getElementById("character-form");
   characterForm.addEventListener("submit", (event) => {
+
     event.preventDefault();
+
+
     const name = document.getElementById("name").value;
+
     const image = document.getElementById("image").value;
+
   
-    const newCharacter = {
-      name,
-      image,
-      votes: 0,
-    };
+    const newCharacter = {name,image,votes: 0,};
   
     
     const span = document.createElement("span");
+
     span.textContent = newCharacter.name;
+
     span.addEventListener("click", () => displayCharacterDetails(newCharacter));
     characterBar.appendChild(span);
   
@@ -129,7 +141,9 @@ document.addEventListener("DOMContentLoaded", () => {
   
     fetch("http://localhost:3000/characters", {
       method: "POST",
+      
       headers: {
+
         "Content-Type": "application/json",
       },
       body: JSON.stringify(newCharacter),
